660b2a2fb5ea6c22fe6d1db41119923ae52cfbd6
lobZter <lobst3rd@gmail.com>
2017-07-11 02:31:49 +0800

    PlanFragment ui

---
+++ app/src/main/java/nctu/cs/cgv/itour/PlanItemAdapter.java
@@ -0,0 +1,36 @@
+package nctu.cs.cgv.itour;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lobZter on 2017/7/10.
+ */
+
+public class PlanItemAdapter extends ArrayAdapter<PlanItem> {
+
+    public PlanItemAdapter(Context context, ArrayList<PlanItem> planItems) {
+        super(context, 0, planItems);
+    }
+
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        PlanItem planItem = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_plan, parent, false);
+        }
+
+        TextView title = (TextView) convertView.findViewById(R.id.title);
+        title.setText(planItem != null ? planItem.title : null);
+
+        return convertView;
+    }
+}
--- app/src/main/java/nctu/cs/cgv/itour/fragment/PlanFragment.java
+++ app/src/main/java/nctu/cs/cgv/itour/fragment/PlanFragment.java
@@ -5,7 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import nctu.cs.cgv.itour.PlanItem;
+import nctu.cs.cgv.itour.PlanItemAdapter;
 import nctu.cs.cgv.itour.R;
 
 public class PlanFragment extends Fragment {
@@ -17,13 +23,20 @@ public class PlanFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        // Defines the xml file for the fragment
         return inflater.inflate(R.layout.fragment_plan, parent, false);
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        // Setup any handles to view objects here
-        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+
+        ArrayList<PlanItem> planItems = new ArrayList<>();
+        PlanItemAdapter planItemAdapter = new PlanItemAdapter(getContext(), planItems);
+        ListView planList = (ListView) view.findViewById(R.id.plan_list);
+        planList.setAdapter(planItemAdapter);
+
+        // testing data
+        planItemAdapter.add(new PlanItem("工程三館", 30));
+        planItemAdapter.add(new PlanItem("第二餐廳", 30));
+        planItemAdapter.add(new PlanItem("浩然圖書館", 30));
     }
 }
