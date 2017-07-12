44d2f3c4ef7fef7533ac4a0c1eb40a169e7ac0f6
lobZter <lobst3rd@gmail.com>
2017-07-12 19:53:48 +0800

    preference fragment

--- app/src/main/java/nctu/cs/cgv/itour/fragment/MapFragment.java
+++ app/src/main/java/nctu/cs/cgv/itour/fragment/MapFragment.java
@@ -19,8 +19,6 @@ import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
--- app/src/main/java/nctu/cs/cgv/itour/fragment/SettingsFragment.java
+++ app/src/main/java/nctu/cs/cgv/itour/fragment/SettingsFragment.java
@@ -1,14 +1,11 @@
 package nctu.cs.cgv.itour.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.preference.PreferenceFragmentCompat;
 
 import nctu.cs.cgv.itour.R;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     public static SettingsFragment newInstance() {
         SettingsFragment fragment = new SettingsFragment();
@@ -16,14 +13,7 @@ public class SettingsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        // Defines the xml file for the fragment
-        return inflater.inflate(R.layout.fragment_settings, parent, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        // Setup any handles to view objects here
-        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preference, rootKey);
     }
 }
