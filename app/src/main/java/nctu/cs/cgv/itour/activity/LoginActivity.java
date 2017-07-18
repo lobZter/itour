8c4f969a51964d0d0890e34052bd6886ada7e9dd
githubuse4206 <zxcvbnm21727@gmail.com>
2017-07-19 01:02:45 +0800

    login & checkin

---
+++ app/src/main/java/nctu/cs/cgv/itour/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package nctu.cs.cgv.itour.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nctu.cs.cgv.itour.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
