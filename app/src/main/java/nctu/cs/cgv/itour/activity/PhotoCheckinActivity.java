43270d3358781130c65d2427a496ea1ecff0a4e2
lobZter <lobst3rd@gmail.com>
2017-08-16 16:19:02 +0800

    image crop

--- app/src/main/java/nctu/cs/cgv/itour/activity/PhotoCheckinActivity.java
+++ app/src/main/java/nctu/cs/cgv/itour/activity/PhotoCheckinActivity.java
@@ -1,56 +1,38 @@
 package nctu.cs.cgv.itour.activity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-import cz.msebera.android.httpclient.Header;
 import nctu.cs.cgv.itour.R;
 
-import static nctu.cs.cgv.itour.MyApplication.fileUploadURL;
+import static nctu.cs.cgv.itour.MyApplication.mapTag;
 import static nctu.cs.cgv.itour.MyApplication.photoPath;
 
 public class PhotoCheckinActivity extends AppCompatActivity {
 
     private static final String TAG = "PhotoCheckinActivity";
-    private String mapTag;
     private float lat = 0;
     private float lng = 0;
     private String filename = " ";
-    // pick image
-    private static final int PICK_PHOTO_FOR_AVATAR = 1024;
     // view objects
     private EditText locationEdit;
     private EditText descriptionEdit;
-    private RelativeLayout photoEdit;
     private ImageView pickedPhoto;
 
     @Override
@@ -62,9 +44,14 @@ public class PhotoCheckinActivity extends AppCompatActivity {
         Intent intent = getIntent();
         lat = intent.getFloatExtra("lat", 0);
         lng = intent.getFloatExtra("lng", 0);
-        mapTag = intent.getStringExtra("mapTag");
 
         setView();
+
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setFixAspectRatio(true)
+                .setAspectRatio(1, 1)
+                .start(this);
     }
 
     @Override
@@ -75,8 +62,8 @@ public class PhotoCheckinActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch(item.getItemId()){
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
             case R.id.btn_submit:
                 String location = locationEdit.getText().toString().trim();
                 String description = descriptionEdit.getText().toString().trim();
@@ -98,67 +85,20 @@ public class PhotoCheckinActivity extends AppCompatActivity {
     private void setView() {
         locationEdit = (EditText) findViewById(R.id.et_location);
         descriptionEdit = (EditText) findViewById(R.id.et_description);
-        photoEdit = (RelativeLayout) findViewById(R.id.et_photo);
-        photoEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pickImage();
-            }
-        });
         pickedPhoto = (ImageView) findViewById(R.id.picked_photo);
-    }
-
-    public void pickImage() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(intent, PICK_PHOTO_FOR_AVATAR);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_PHOTO_FOR_AVATAR && resultCode == Activity.RESULT_OK) {
-            if (data == null) {
-                //Display an error
-                return;
-            }
-
-            try {
-                InputStream inputStream = getContentResolver().openInputStream(data.getData());
-                //Now you can do whatever you want with your inpustream, save it as file, upload to a server, decode a bitmap...
-                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                filename = resultUri.getPath();
+                Bitmap bitmap = BitmapFactory.decodeFile(resultUri.getPath());
                 pickedPhoto.setImageBitmap(bitmap);
-
-                filename = photoPath + new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime()) + ".png";
-
-                FileOutputStream outFile = null;
-                try {
-                    outFile = new FileOutputStream(filename);
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outFile); // bmp is your Bitmap instance
-                    // PNG is a lossless format, the compression factor (100) is ignored
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        if (outFile != null) {
-                            outFile.close();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-//
-//                // save file
-//                byte[] buffer = new byte[inputStream.available()];
-//                inputStream.read(buffer);
-//
-//
-//                File targetFile = new File(filename);
-//                OutputStream outStream = new FileOutputStream(targetFile);
-//                outStream.write(buffer);
-
-            } catch (IOException e) {
-                e.printStackTrace();
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
             }
         }
     }
