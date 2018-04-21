8fb388720e3e1d2846496a7c3647c6e95994ed82
lobZter <lobst3rd@gmail.com>
2018-04-22 00:27:32 +0800

    logout and stopService

--- app/src/main/java/nctu/cs/cgv/itour/fragment/SettingsFragment.java
+++ app/src/main/java/nctu/cs/cgv/itour/fragment/SettingsFragment.java
@@ -17,6 +17,7 @@ import nctu.cs.cgv.itour.R;
 import nctu.cs.cgv.itour.activity.LoginActivity;
 import nctu.cs.cgv.itour.activity.RegisterActivity;
 import nctu.cs.cgv.itour.service.AudioFeedbackService;
+import nctu.cs.cgv.itour.service.CheckinNotificationService;
 import nctu.cs.cgv.itour.service.ScreenShotService;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -62,11 +63,11 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 @Override
                 public boolean onPreferenceClick(Preference arg0) {
                     FirebaseAuth.getInstance().signOut();
-                    getContext().stopService(new Intent(getContext(), AudioFeedbackService.class));
-                    getContext().stopService(new Intent(getContext(), ScreenShotService.class));
 
                     getActivity().finish();
+                    getContext().stopService(new Intent(getContext(), ScreenShotService.class));
                     getContext().stopService(new Intent(getContext(), AudioFeedbackService.class));
+                    getContext().stopService(new Intent(getContext(), CheckinNotificationService.class));
                     startActivity(new Intent(getContext(), LoginActivity.class));
                     return true;
                 }
